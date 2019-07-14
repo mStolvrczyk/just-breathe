d16c821f561201bfeae6ac1da7e82c570a29daf6
Marcin Stolarczyk <m.stolvrczyk@gmail.com>
2019-07-14 09:12:22 +0200

    configured main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import router from './router'
+import VueRouter from 'vue-router'
+import routes from './routes'
 import store from './store'
 import './registerServiceWorker'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: routes
+})
+
 new Vue({
-  router,
-  store,
+  router: router,
+  store: store,
   render: h => h(App)
 }).$mount('#app')
