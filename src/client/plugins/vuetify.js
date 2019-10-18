5256cf528878e24c452936d71ab402f9bca1d235
Marcin Stolarczyk <m.stolvrczyk@gmail.com>
2019-10-18 10:04:56 +0200

    refactoring

--- src/client/plugins/vuetify.js
+++ src/client/plugins/vuetify.js
@@ -4,7 +4,10 @@ import theme from './theme'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify, {
-  iconfont: 'mdi',
-  theme
-})
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+});
