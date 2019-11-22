dd039982452023fbf1b571cde7152f6f54b2de7c
Marcin Stolarczyk <m.stolvrczyk@gmail.com>
2019-11-22 10:59:54 +0100

    working on enforcing https

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 8000
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist/client'))
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next()
+    }
+  })
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'))
