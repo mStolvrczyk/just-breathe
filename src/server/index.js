6c0f6d2fff303661bcb7b6db786d2e9d77410de5
Marcin <m.stolvrczyk@gmail.com>
2019-11-24 14:28:22 +0100

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,9 @@ const app = require('./app')
 const PORT = process.env.PORT || 8000
 
 // if (process.env.NODE_ENV === 'production') {
-//
+//   app.use(express.static('dist/client'))
 //   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'))
 //   })
 // }
 
@@ -14,9 +15,10 @@ app.get('*', function (req, res, next) {
     app.use(express.static('dist/client'))
     res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'))
   }
-})
-app.use(express.static(path.resolve(__dirname, '../client')))
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
+  // if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
 //   res.redirect('https://'+req.hostname+req.url)
 // else
 //   next() /* Continue to other routes if we're not redirecting */
+})
+app.use(express.static(path.resolve(__dirname, '../client')))
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
