af43582a3d3d7c31241d3b33ff8a9acb620d93c5
Marcin <m.stolvrczyk@gmail.com>
2019-11-24 16:10:15 +0100

    testing

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const userController = require('./controllers/userController.js')
+const sslRedirect = require('heroku-ssl-redirect')
+
+app.use(sslRedirect())
 
 app.all('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
--- src/server/index.js
+++ src/server/index.js
@@ -6,17 +6,8 @@ const PORT = process.env.PORT || 8000
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist/client'))
   app.get('*', (req, res) => {
-    if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
-      res.redirect('https://' + req.hostname + req.url)
-    }
     res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'))
   })
 }
 app.use(express.static(path.resolve(__dirname, '../client')))
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
-
-// app.get('*', function(req,res,next) {
-//   if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-//   else
-//     next() /* Continue to other routes if we're not redirecting */
-// });
