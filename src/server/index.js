92bff9d2d739d8f7f8d3211ebc14880b7830cda7
Marcin <m.stolvrczyk@gmail.com>
2019-11-24 12:12:55 +0100

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -3,20 +3,11 @@ const path = require('path')
 const app = require('./app')
 const PORT = process.env.PORT || 8000
 
-// if (process.env.NODE_ENV === 'production') {
-//
-//   app.get('*', (req, res) => {
-//   })
-// }
-
-app.get('*', function (req, res, next) {
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('dist/client'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist/client'))
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'))
-  }
-})
+  })
+}
 app.use(express.static(path.resolve(__dirname, '../client')))
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
-//   res.redirect('https://'+req.hostname+req.url)
-// else
-//   next() /* Continue to other routes if we're not redirecting */
