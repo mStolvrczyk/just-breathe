6941d22d3c6a84ee67e6a36402f0e92853dfb53f
Marcin <m.stolvrczyk@gmail.com>
2020-04-18 12:04:26 +0200

    testing deployment

--- src/services/StationsService.js
+++ src/services/StationsService.js
@@ -1,16 +1,12 @@
-// import appHttpClient from '../libs/appHttpClient'
+import appHttpClient from '../libs/appHttpClient'
 import { prop } from 'ramda'
-import axios from 'axios'
+
 export default class StationsService {
   async getAll () {
-    return axios.get('https://just-breathe-app.netlify.app/.netlify/functions/server/stations').then(prop('data'))
+    return appHttpClient.get('/stations').then(prop('data'))
   }
 
   async getStation (id) {
-    return axios.get(`https://just-breathe-app.netlify.app/.netlify/functions/server/stations/${id}`).then(prop('data'))
+    return appHttpClient.get(`/stations/${id}`).then(prop('data'))
   }
-
-  // async getSensor (id) {
-  //   return appHttpClient.get(`/sensors/${id}`).then(prop('data'))
-  // }
 }
