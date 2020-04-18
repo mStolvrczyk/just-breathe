202be7b28e4977640d07fd5e1e94037e5db7674e
Marcin <m.stolvrczyk@gmail.com>
2020-04-18 12:20:59 +0200

    testing deployment

--- src/express/server.js
+++ src/express/server.js
@@ -42,11 +42,11 @@ const userController = require('./controllers/userController.js')
 require('tls').DEFAULT_MIN_VERSION = 'TLSv1'
 app.use(cors())
 app.use(bodyParser.json())
-app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  next()
-})
+// app.all('/*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+//   next()
+// })
 app.use('/.netlify/functions/server', userController)
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../../public/index.html')))
 
