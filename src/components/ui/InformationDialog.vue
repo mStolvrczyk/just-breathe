e3b9a63c8dc7375020583b15dc6560d22e9a66af
Marcin <m.stolvrczyk@gmail.com>
2020-04-23 15:35:09 +0200

    refactoring

--- src/components/ui/InformationDialog.vue
+++ src/components/ui/InformationDialog.vue
@@ -7,14 +7,13 @@
     <div id="information-dialog-card">
       <h3 class="information-info">{{information}}</h3>
       <div class="row chart button">
-        <v-btn @click="closeInformationDialog" class="teal--text font-weight-bold" rounded color="#EEEEEE" dark>{{ buttonText }}</v-btn>
+        <v-btn @click="closeInformationDialog" class="teal--text font-weight-bold" rounded color="#EEEEEE" dark>OK</v-btn>
       </div>
     </div>
   </v-dialog>
 </template>
 
 <script>
-  // fix
     export default {
       name: 'InformationDialog',
       props: {
@@ -24,19 +23,12 @@
       methods: {
         closeInformationDialog () {
           this.$emit('closeInformationDialog', { informationDialogVisibility: false, informationDialogText: null })
-          if (this.information === 'Dostępna jest nowa wersja aplikacji') {
+          if (this.information === 'Aplikacja została zaktualizowana.') {
             window.location.reload(true)
           }
         }
       },
       computed: {
-        buttonText () {
-          if (this.information === 'Dostępna jest nowa wersja aplikacji') {
-            return 'Pobierz'
-          } else {
-            return 'OK'
-          }
-        },
         informationDialogWidth () {
           if (this.$vuetify.breakpoint.xsOnly) {
             return 60 + '%'
