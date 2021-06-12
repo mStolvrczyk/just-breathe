d3649749e2abf77fe559f5e8d2a53ec33fe4b8cc
Marcin <m.stolvrczyk@gmail.com>
2021-06-12 09:14:17 +0200

    lambda functions fix

---
+++ dist/precache-manifest.383c18720d506b5bea12e46dd6b51a49.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f9e725b92ade4bfa20",
+    "url": "/css/app.84d454ef.css"
+  },
+  {
+    "revision": "d0f9e725b92ade4bfa20",
+    "url": "/js/app.944ac3ce.js"
+  },
+  {
+    "revision": "83b5ecfa4e1ffc20377e",
+    "url": "/css/chunk-vendors.085d2249.css"
+  },
+  {
+    "revision": "83b5ecfa4e1ffc20377e",
+    "url": "/js/chunk-vendors.fdc08593.js"
+  },
+  {
+    "revision": "8e1bc0ac809d4dada032df186ecf56f8",
+    "url": "/img/fog-yellow.8e1bc0ac.png"
+  },
+  {
+    "revision": "d617d863c9acf9c2058fab7da1e09c13",
+    "url": "/img/wind.d617d863.png"
+  },
+  {
+    "revision": "7dac487bcd6a92e3bf63887777f17b56",
+    "url": "/img/clock.7dac487b.png"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "2f6994b940556f17ed464babdcf24605",
+    "url": "/img/humidity.2f6994b9.png"
+  },
+  {
+    "revision": "3e16096f09aee5f9e59042849ae1be38",
+    "url": "/img/jb-logo.3e16096f.png"
+  },
+  {
+    "revision": "52ef243ca478003849c0bb55a7f505c2",
+    "url": "/img/jb-sygnet.52ef243c.png"
+  },
+  {
+    "revision": "8f8c421c03462d73fbde6cd936be24e1",
+    "url": "/img/appImage.8f8c421c.jpg"
+  },
+  {
+    "revision": "46d04706ef9682ce97cf6484ea2c4bac",
+    "url": "/img/place-yellow.46d04706.png"
+  },
+  {
+    "revision": "29e8a99b919644355fc6842acef08dcc",
+    "url": "/img/road-yellow.29e8a99b.png"
+  },
+  {
+    "revision": "b0ed89684233a441e0207367e7be1e46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24451388c2fc69419fac47ca08311257",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
